37443e3e34678ffdba0fad7eca4ddb9612a3fb74
Gabrieldev28 <gabrieldev28@gmail.com>
2026-09-01 11:57:26 -0300

    iniciado o tratamento do profileview, e refinamento do headernav

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -6,22 +6,21 @@
   >
     <!-- Sol -->
     <span
-      class="flex items-center px-5 py-3 rounded-full transition-all duration-300"
+      class="flex items-center justify-center w-12 h-12 rounded-full transition-all duration-300"
       :class="theme === 'light' ? 'glass glass-light-toggle' : ''"
     >
-      <SunIcon class="w-4" />
+      <SunIcon class="w-4 h-4" />
     </span>
 
     <!-- Lua -->
     <span
-      class="flex items-center px-5 py-3 rounded-full transition-all duration-300"
+      class="flex items-center justify-center w-12 h-12 rounded-full transition-all duration-300"
       :class="theme === 'dark' ? 'glass glass-dark-toggle' : ''"
     >
-      <MoonIcon class="w-4" />
+      <MoonIcon class="w-4 h-4" />
     </span>
   </div>
 </template>
-
 <script setup lang="ts">
 import SunIcon from './icons/SunIcon.vue'
 import MoonIcon from './icons/MoonIcon.vue'
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+<div class="w-10/12">
+    <HeaderNav />
+    <div class="w-full">
+        <p>teste</p>
+    </div>
+</div>
+
+</template>
+
+<script setup lang="ts">
+import HeaderNav from '@/components/HeaderNav.vue';
+
+</script>
